ab73a0755f4a7e832376271e0be8d13ebc2d8294
Sayantan Kapat <ksayantan1823@gmail.com>
2021-06-28 13:04:50 +0530

    initialize landing section

---
+++ portfolio/src/components/Landing-section/LandingSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingSection() {
+    return (
+        <div>
+            <h1>Landing Section</h1>
+        </div>
+    )
+}
+
+export default LandingSection;

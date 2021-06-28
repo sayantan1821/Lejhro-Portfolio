139cb7894ac745ae45cacc4e928ae2f420622b50
Sayantan Kapat <ksayantan1823@gmail.com>
2021-06-29 02:46:55 +0530

    update landing section with text

--- portfolio/src/components/Landing-section/LandingSection.js
+++ portfolio/src/components/Landing-section/LandingSection.js
@@ -1,12 +1,32 @@
 import React from 'react'
-import imgf from '../../assets/Test.svg';
+import CompPhoto from '../../assets/Test.svg';
+import Typical from 'react-typical'
 import './LandingSection.css';
+import logo from '../../assets/logo.svg';
 
 function LandingSection() {
     return (
         <div className='landing-section'>
-            <img src='../../assets/Test.svg' />
+            <img className='nav-logo' src={logo} alt='logo' />
+            <img className='working-img' src={CompPhoto} alt= 'computer'/>
+            <div className='head-text'>
+                <h1>
+                    Hey Stranger! Welcome to my Portfolio
+                </h1>
+                <h3>Myself Sayantan Kapat
+                    <Typical
+                        steps={[
+                            'A Web Developer', 1500, 
+                            'A MERN Developer', 1500, 
+                            'A ML Engineer', 1500, 
+                            'A Problem solver', 1500]}
+                        loop={Infinity}
+                        wrapper="p"
+                    />
+                </h3>        
+            </div>
         </div>
+        
     )
 }
 

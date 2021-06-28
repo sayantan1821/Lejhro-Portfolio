e52846f0b6383de7d2e24031a905cb5316616e0c
sairambalu <balusairam26@gmail.com>
2021-06-28 18:55:40 +0530

    temporary project file

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,10 +2,12 @@ import React from "react"
 import './App.css';
 import Testimonials from "./components/Testimonials";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
+      <Projects/>
      <Testimonials/>
     </div>
   );
--- portfolio/src/components/Testimonials.js
+++ portfolio/src/components/Testimonials.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import pic1 from "../images/pic1.jpg"
 import "../styles/Testimonials.css"
+import facebook from "../images/facebook.png"
+import instagram from "../images/instagram.png"
+import twitter from "../images/twitter.png"
 
 function Testimonials() {
 
     return (
         <section className="testimonials">
            <h1>Testimonials</h1> 
-    <div className="container">
+    <div className="testimonial_container">
     <div className="card">
     <div className="top">
         <img src={pic1} alt="profile" className="left"/>
     </div>
-    <div className="social"></div>
+    <div className="social">
+      <a href="https://www.flaticon.com/search?word=twitter&type=icon" alt="social link">  <img src={facebook} alt="icon" width="30px" height="30px"/></a>
+      <a href="https://www.flaticon.com/search?word=twitter&type=icon" alt="social link">  <img src={instagram} alt="icon" width="30px" height="30px"/></a>
+      <a href="https://www.flaticon.com/search?word=twitter&type=icon" alt="social link">  <img src={twitter} alt="icon" width="30px" height="30px" /></a>
+    </div>
     <div className="center">
     We provide react-bootstrap.js and react-bootstrap.min.js bundles with all components exported on the window.ReactBootstrap object.
      These bundles are available on unpkg, as well as in the npm package.
@@ -23,6 +30,11 @@ function Testimonials() {
     <div className="top">
         <img src={pic1} alt="profile" className="left"/>
     </div>
+    <div className="social">
+      <a href="https://www.flaticon.com/search?word=twitter&type=icon" alt="social link">  <img src={facebook} alt="icon" width="30px" height="30px"/></a>
+      <a href="https://www.flaticon.com/search?word=twitter&type=icon" alt="social link">  <img src={instagram} alt="icon" width="30px" height="30px"/></a>
+      <a href="https://www.flaticon.com/search?word=twitter&type=icon" alt="social link">  <img src={twitter} alt="icon" width="30px" height="30px" /></a>
+    </div>
     <div className="center">
     We provide react-bootstrap.js and react-bootstrap.min.js bundles with all components exported on the window.ReactBootstrap object.
      These bundles are available on unpkg, as well as in the npm package.
@@ -32,6 +44,11 @@ function Testimonials() {
     <div className="card">
     <div className="top">
         <img src={pic1} alt="profile" className="left"/>
+    </div>
+    <div className="social">
+      <a href="https://www.flaticon.com/search?word=twitter&type=icon" alt="social link">  <img src={facebook} alt="icon" width="30px" height="30px"/></a>
+      <a href="https://www.flaticon.com/search?word=twitter&type=icon" alt="social link">  <img src={instagram} alt="icon" width="30px" height="30px"/></a>
+      <a href="https://www.flaticon.com/search?word=twitter&type=icon" alt="social link">  <img src={twitter} alt="icon" width="30px" height="30px" /></a>
     </div>
     <div className="center">
     We provide react-bootstrap.js and react-bootstrap.min.js bundles with all components exported on the window.ReactBootstrap object.
